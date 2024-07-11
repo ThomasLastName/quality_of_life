1033663caeb4f99136ab58565f39eaeb0a58b544
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-10 19:41:07 -0600

    Create __init__.py with deprecation warning

---
+++ __init__.py
@@ -0,0 +1,5 @@
+import warnings
+warnings.warn(
+    "This version is deprecated and no longer being maintaned! Please instead see https://github.com/ThomasLastName/quality-of-life,
+    DeprecationWarning
+)
