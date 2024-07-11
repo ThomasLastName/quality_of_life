1bb9f206c5b9f9ad6eceee4fc3650b9248062676
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-10 19:51:09 -0600

    Added deprecation warning

--- my_torchvision_utils.py
+++ my_torchvision_utils.py
@@ -1,3 +1,10 @@
+import warnings
+warnings.warn(
+    "This version is deprecated and no longer being maintaned! Please instead see https://github.com/ThomasLastName/quality-of-life",
+    DeprecationWarning
+)
+
+
 import torch
 import torchvision
 
