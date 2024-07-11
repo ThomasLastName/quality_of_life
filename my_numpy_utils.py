3e6799d8aceebbe7451e541f7ab39797d1d4f1ec
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-10 19:50:29 -0600

    Added deprecation warning

--- my_numpy_utils.py
+++ my_numpy_utils.py
@@ -1,4 +1,10 @@
 
+import warnings
+warnings.warn(
+    "This version is deprecated and no longer being maintaned! Please instead see https://github.com/ThomasLastName/quality-of-life",
+    DeprecationWarning
+)
+
 # ~~~ Tom Winckelman wrote this; maintained at: https://github.com/ThomasLastName/quality_of_life
 
 import numpy as np
