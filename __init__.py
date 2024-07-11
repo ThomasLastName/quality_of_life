9d9fd9df2e6ce46895b7be2d781e075fb2b2de94
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-10 19:41:30 -0600

    Fixed dumb error

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 import warnings
 warnings.warn(
-    "This version is deprecated and no longer being maintaned! Please instead see https://github.com/ThomasLastName/quality-of-life,
+    "This version is deprecated and no longer being maintaned! Please instead see https://github.com/ThomasLastName/quality-of-life",
     DeprecationWarning
 )
