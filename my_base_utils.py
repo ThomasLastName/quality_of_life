56c027b877ee57617ef8fd245d23c51f6a6293e3
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-10 19:48:08 -0600

    Added deprecation warning

--- my_base_utils.py
+++ my_base_utils.py
@@ -1,3 +1,8 @@
+import warnings
+warnings.warn(
+    "This version is deprecated and no longer being maintaned! Please instead see https://github.com/ThomasLastName/quality-of-life",
+    DeprecationWarning
+)
 
 # ~~~ Tom Winckelman wrote this; maintained at: https://github.com/ThomasLastName/quality_of_life
 
