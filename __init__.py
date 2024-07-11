769f4e7a3dd364829cd3efedc75eacf2fa067551
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-10 19:52:19 -0600

    Update __init__.py
    Tried to make the deprecation warning print

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 import warnings
-warnings.warn(
-    "This version is deprecated and no longer being maintaned! Please instead see https://github.com/ThomasLastName/quality-of-life",
-    DeprecationWarning
-)
+def foo():
+    warnings.warn(
+        "This version is deprecated and no longer being maintaned! Please instead see https://github.com/ThomasLastName/quality-of-life",
+        DeprecationWarning
+    )
+
+foo()
